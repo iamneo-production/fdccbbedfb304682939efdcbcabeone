b9cf4c77913bbfa030adb518a725e165da0fb5b4
Iamneo <techadmin@iamneo.ai>
2023-09-03 18:23:25 +0000

    Backup Commit

--- public/script.js
+++ public/script.js
@@ -1,66 +1,45 @@
+// Initial game state
+let cells = ['', '', '', '', '', '', '', '', ''];
 let currentPlayer = 'X';
-let isGameActive = true;
+let result = document.querySelector('.result');
+let btns = document.querySelectorAll('.btn');
+let conditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-function makeMove(button) {
-    if (isGameActive && button.value === '') {
-            button.value = currentPlayer;
-                    button.disabled = true;
-                            button.textContent = currentPlayer;
-                                    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-                                            updateResult();
-                                                }
-                                                }
+// Function to handle player moves
+function ticTacToe(btn, i) {
+        if (cells[i] === '' && !checkWin()) {
+                cells[i] = currentPlayer;
+                        btn.textContent = currentPlayer;
+                                
+                                        const winner = checkWin();
+                                                if (winner) {
+                                                            result.textContent = `Player ${winner} wins!`;
+                                                                    } else {
+                                                                                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+                                                                                        }
+                                                                                            }
+                                                                                            }
 
-                                                function updateResult() {
-                                                    const resultContainer = document.getElementById('result');
-                                                        const winner = checkWinner();
+                                                                                            // Function to reset the game
+                                                                                            function resetGame() {
+                                                                                                cells = ['', '', '', '', '', '', '', '', ''];
+                                                                                                    currentPlayer = 'X';
+                                                                                                        result.textContent = '';
+                                                                                                            btns.forEach((btn) => {
+                                                                                                                    btn.textContent = '';
+                                                                                                                        });
+                                                                                                                        }
 
-                                                            if (winner) {
-                                                                    resultContainer.textContent = `Player ${winner} wins!`;
-                                                                            isGameActive = false;
-                                                                                } else if ([...document.querySelectorAll('.btn')].every(button => button.value !== '')) {
-                                                                                        resultContainer.textContent = "It's a draw!";
-                                                                                                isGameActive = false;
-                                                                                                    } else {
-                                                                                                            resultContainer.textContent = `Player ${currentPlayer}'s Turn`;
-                                                                                                                }
-                                                                                                                }
-
-                                                                                                                function checkWinner() {
-                                                                                                                    const winningCombinations = [
-                                                                                                                            [0, 1, 2],
-                                                                                                                                    [3, 4, 5],
-                                                                                                                                            [6, 7, 8],
-                                                                                                                                                    [0, 3, 6],
-                                                                                                                                                            [1, 4, 7],
-                                                                                                                                                                    [2, 5, 8],
-                                                                                                                                                                            [0, 4, 8],
-                                                                                                                                                                                    [2, 4, 6],
-                                                                                                                                                                                        ];
-
-                                                                                                                                                                                            for (const combination of winningCombinations) {
-                                                                                                                                                                                                    const [a, b, c] = combination;
-                                                                                                                                                                                                            if (document.querySelector(`.btn[value="${a}"]`).value &&
-                                                                                                                                                                                                                        document.querySelector(`.btn[value="${a}"]`).value === document.querySelector(`.btn[value="${b}"]`).value &&
-                                                                                                                                                                                                                                    document.querySelector(`.btn[value="${a}"]`).value === document.querySelector(`.btn[value="${c}"]`).value) {
-                                                                                                                                                                                                                                                return document.querySelector(`.btn[value="${a}"]`).value;
-                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                            }
-
-                                                                                                                                                                                                                                                                return null;
-                                                                                                                                                                                                                                                                }
-
-                                                                                                                                                                                                                                                                function resetGame() {
-                                                                                                                                                                                                                                                                    const buttons = document.querySelectorAll('.btn');
-                                                                                                                                                                                                                                                                        buttons.forEach(button => {
-                                                                                                                                                                                                                                                                                button.value = '';
-                                                                                                                                                                                                                                                                                        button.disabled = false;
-                                                                                                                                                                                                                                                                                                button.textContent = '';
-                                                                                                                                                                                                                                                                                                    });
-                                                                                                                                                                                                                                                                                                        currentPlayer = 'X';
-                                                                                                                                                                                                                                                                                                            isGameActive = true;
-                                                                                                                                                                                                                                                                                                                document.getElementById('result').textContent = `Player ${currentPlayer}'s Turn`;
-                                                                                                                                                                                                                                                                                                                }
-
-                                                                                                                                                                                                                                                                                                                document.getElementById('reset-button').addEventListener('click', resetGame);
-                                                                                                                                                                                                                                                                                                                
+                                                                                                                        // Add event listeners to buttons
+                                                                                                                        btns.forEach((btn, i) => {
+                                                                                                                            btn.addEventListener('click', () =>
+}
